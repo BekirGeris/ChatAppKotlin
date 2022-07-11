7e48142dc2a0a572ce6d4752402e773c513ac641
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2022-07-12 00:46:30 +0300

    commit

--- app/src/main/java/com/example/chatappkotlin/view/fragment/ChatFragment.kt
+++ app/src/main/java/com/example/chatappkotlin/view/fragment/ChatFragment.kt
@@ -5,19 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.text.set
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.chatappkotlin.R
 import com.example.chatappkotlin.databinding.FragmentChatBinding
 import com.example.chatappkotlin.view.adapter.ChatAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class ChatFragment : Fragment() {
 
     lateinit var binding: FragmentChatBinding
     lateinit var adapter: ChatAdapter
+    val list = ArrayList<String>()
+
+    lateinit var auth: FirebaseAuth
+    lateinit var database: FirebaseDatabase
+    lateinit var databaseReferance: DatabaseReference
+
+    lateinit var user: FirebaseUser
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReferance = database.reference
+        user = auth.currentUser!!
     }
 
     override fun onCreateView(
@@ -25,16 +44,53 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentChatBinding.inflate(layoutInflater)
+        binding.send.setOnClickListener { sendMessage(binding.messageEditText.text.toString()) }
 
-        val list = ArrayList<String>()
-        list.add("dfvfdvfdvfd")
-        list.add("dfvfdvfd")
-        list.add("dfvfdvfdvytufkyfd")
         adapter = ChatAdapter(list)
+
+        getData()
 
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
         binding.recyclerView.adapter = adapter
 
         return binding.root
+    }
+
+    fun sendMessage(message: String) {
+        val uuid = UUID.randomUUID().toString()
+
+        databaseReferance.child("chats").child(uuid).child("usermessage").setValue(message)
+        databaseReferance.child("chats").child(uuid).child("useremail").setValue(user.email)
+        databaseReferance.child("chats").child(uuid).child("time").setValue(ServerValue.TIMESTAMP)
+
+        binding.messageEditText.text.clear()
+        getData()
+    }
+
+    fun getData() {
+        val newReference = database.getReference("chats")
+
+        val query: Query = newReference.orderByChild("time")
+
+        query.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                list.clear()
+                for (ds in snapshot.children) {
+                    val hashMap = ds.value as HashMap<String, String>
+                    val userEmail: String? = hashMap.get("useremail")
+                    val userMessage: String? = hashMap.get("usermessage")
+
+                    if (userEmail != null) {
+                        list.add(userEmail.split("@").get(0) + ": " + userMessage)
+                    }
+
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(activity, error.message, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/chatappkotlin/view/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/chatappkotlin/view/fragment/ProfileFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.chatappkotlin.R
+import com.example.chatappkotlin.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    lateinit var binding: FragmentProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,9 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+
 }
