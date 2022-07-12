2d45f72ae7bce918b0d04f489846d1c2f29b52e9
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2022-07-12 22:50:39 +0300

    profile işlemleri

--- app/src/main/java/com/example/chatappkotlin/view/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/chatappkotlin/view/fragment/ProfileFragment.kt
@@ -1,16 +1,12 @@
 package com.example.chatappkotlin.view.fragment
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,10 +14,17 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.chatappkotlin.R
+import androidx.navigation.fragment.findNavController
 import com.example.chatappkotlin.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.HashMap
 
 class ProfileFragment : Fragment(), View.OnClickListener {
 
@@ -32,6 +35,19 @@ class ProfileFragment : Fragment(), View.OnClickListener {
     lateinit var requestMultiplePermissions: ActivityResultLauncher<Array<String>>
 
     var selectedUri: Uri? = null
+
+    lateinit var database: FirebaseDatabase
+    lateinit var databaseReference: DatabaseReference
+    lateinit var storage: StorageReference
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database.reference
+        storage = FirebaseStorage.getInstance().reference
+        auth = FirebaseAuth.getInstance()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -70,6 +86,8 @@ class ProfileFragment : Fragment(), View.OnClickListener {
             }
         }
 
+        getData()
+
         return binding.root
     }
 
@@ -90,15 +108,60 @@ class ProfileFragment : Fragment(), View.OnClickListener {
             }
         } else if (v == binding.update) {
             if (selectedUri != null && userName.isNotEmpty()) {
-
+                upload(userName)
             } else {
                 Toast.makeText(context, "Bilgileri giriniz.", Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    fun goToGallery() {
+    private fun upload(userName: String) {
+        val imageName = UUID.randomUUID().toString()
+        val newReference = storage.child("images/$imageName.jpg")
+        newReference.putFile(selectedUri!!)
+            .addOnSuccessListener { task ->
+                val profileImageReference = FirebaseStorage.getInstance().getReference("images/$imageName.jpg")
+                profileImageReference.downloadUrl.addOnSuccessListener { uploadedImageUrl ->
+                    val profileInfoName = UUID.randomUUID().toString()
+                    databaseReference.child("profiles").child(profileInfoName).child("useremail").setValue(auth.currentUser!!.email.toString())
+                    databaseReference.child("profiles").child(profileInfoName).child("username").setValue(userName)
+                    databaseReference.child("profiles").child(profileInfoName).child("profileImageUri").setValue(uploadedImageUrl.toString())
+                    findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToNav2())
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+    }
+
+    private fun goToGallery() {
         getContent.launch("image/*")
+    }
+    private fun getData() {
+        val newReference = database.getReference("profiles")
+
+        newReference.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (s in snapshot.children) {
+                    val hashMap: HashMap<String, String> = s.value as HashMap<String, String>
+                    val userEmail = hashMap.get("useremail")
+
+                    if (userEmail != null && auth.currentUser != null && userEmail.equals(auth.currentUser!!.email.toString())) {
+                        val userName = hashMap.get("username")
+                        val userImageUri = hashMap.get("profileImageUri")
+
+                        if (userName != null && userImageUri != null) {
+                            binding.textView.setText(userName)
+                            Picasso.get().load(userImageUri).into(binding.profile);
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
 
